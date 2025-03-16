feabd67d2a92e5f1f31a9ca9de79c1b25a7a7e15
etaoko333 <131201117+etaoko333@users.noreply.github.com>
2025-03-16 20:09:18 +0000

    Create SkyebankAppApplication.java

---
+++ SkyebankAppApplication.java
@@ -0,0 +1,11 @@
+package com.example.skyebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyebankAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SkyebankAppApplication.class, args);
+    }
+}
